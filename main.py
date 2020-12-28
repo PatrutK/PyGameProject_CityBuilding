8864c12ab452bcdab8a640fafc0ee3ed35b0ba22
PatrutK <utkin.best.no@gmail.com>
2020-12-28 20:37:22 +0300

    Редакировали код, изменили размер экрана

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-import sys
-import time
-import random
-from PyQt5 import *  # QtCore, QtGui, QtWidgets
-import pygame  # from pygame.locals import *
-
-
-# class MainMenu(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
-#         uic.loadUi("PROJECT.ui", self)
+import pygame
 
 
 class Board:  # отрисовка поля
-    def __init__(self, width, height, left=5, top=5, cell_size=20):  # так удобнее
+    def __init__(self, width, height, left=10, top=10, cell_size=20):  # так удобнее
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
@@ -39,19 +29,13 @@ class Board:  # отрисовка поля
             self.board[self.cell_y][self.cell_x] = (self.board[self.cell_y][self.cell_x] + 1)
 
 
-class ResidentialBuildings:  # здесь будут происходить основные действия, вплане геймплея - случайные события, стастика, игровые дни и т.д.
+class ResidentialBuildings(Board):
     def __init__(self, *args, **k_args):
         super().__init__()
         pass
 
-    def get_click(self, mouse_position):
-        cell = self.get_cell(mouse_position)
-        self.on_click(cell)
-        pygame.draw.rect(screen, (255, 255, 255),
-                         (event.pos[0], event.pos[1], self.cell_size * 4, self.cell_size * 3))
 
-
-class IndustrialBuildings:
+class IndustrialBuildings(Board):
     def __init__(self, *args):
         super().__init__()
         pass
@@ -81,19 +65,13 @@ class Road(Board):
 
 if __name__ == '__main__':
     pygame.init()
-    size = 0, 1  # не нужная переменная, вернее нужная, но у нас же fullscreen!
-    screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-    board = Board(35, 63, 10, 10, 20)
+    size = 1920, 1000
+    screen = pygame.display.set_mode(size)
+    board = Board(49, 95)
     homes = ResidentialBuildings
-    pygame.display.set_caption('Панельки')  # это не нужно, ибо у нас fullscreen (название не видно)
+    pygame.display.set_caption('Панельки')
     running = True
     main_build = True  # режим наблюдателя
-    build_1 = False
-    build_2 = False
-    build_3 = False
-    build_4 = False
-    build_5 = False
-    build_6 = False
 
     while running:
         for event in pygame.event.get():
@@ -112,50 +90,5 @@ if __name__ == '__main__':
             if main_build:
                 board.render(screen)
                 pygame.display.flip()
-            # if not main_build:
-            #     screen.fill((0, 0, 0))
-            #     pygame.display.flip()
-        if keys[pygame.K_1]:
-            build_1 = True
-            build_2, build_3, build_4, build_5, build_6 = False
-        # ВЫБРАТЬ постройку - далее будем кликать мышкой на поле (поле, дом, завод, кино)
-        if keys[pygame.K_2]:
-            build_2 = True
-            build_1, build_3, build_4, build_5, build_6 = False
 
-        if keys[pygame.K_3]:
-            build_3 = True
-            build_1, build_2, build_4, build_5, build_6 = False
-
-        if keys[pygame.K_4]:
-            build_4 = True
-            build_1, build_2, build_3, build_5, build_6 = False
-
-        if keys[pygame.K_5]:
-            build_5 = True
-            build_1, build_2, build_3, build_4, build_6 = False
-
-        if keys[pygame.K_6]:
-            build_6 = True
-            build_1, build_2, build_3, build_4, build_5 = False
-
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN and build_1:  # Постройка зданий - завод
-                homes.get_click()
-                pygame.display.flip()
-            elif event.type == pygame.MOUSEBUTTONDOWN and build_2:  # Постройка зданий - дом
-                board.get_click(event.pos)
-                pygame.display.flip()
-            elif event.type == pygame.MOUSEBUTTONDOWN and build_3:  # Постройка зданий - кинотеатр
-                board.get_click(event.pos)
-                pygame.display.flip()
-            elif event.type == pygame.MOUSEBUTTONDOWN and build_4:  # Постройка зданий - поле (ферма)
-                board.get_click(event.pos)
-                pygame.display.flip()
-            elif event.type == pygame.MOUSEBUTTONDOWN and build_5:  # Постройка зданий - банк
-                board.get_click(event.pos)
-                pygame.display.flip()
-            elif event.type == pygame.MOUSEBUTTONDOWN and build_6:  # Постройка зданий - музей
-                board.get_click(event.pos)
-                pygame.display.flip()
     pygame.quit()
