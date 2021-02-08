9c2d92ee62870b5bf9868d13db11b59cf9400cc0
PatrutK <utkin.best.no@gmail.com>
2021-02-08 21:29:24 +0300

    Add test button, import pygame_gui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import pygame_gui
 import sys
 
 all_sprites = pygame.sprite.Group()
@@ -22,18 +23,18 @@ def load_image(name, color_key=None):  # нужна для подгрузки к
 
 class Board:  # отрисовка поля
     def __init__(self, screen):  # так удобнее
-        self.width = 50
-        self.height = 100
-        self.board = [[0] * self.width for _ in range(self.height)]
+        self.height = 42
+        self.width = 70
+        self.board = [[0] * self.height for _ in range(self.width)]
         self.left = 10
-        self.top = 10
+        self.top = 60
         self.cell_size = 20
         self.screen = screen
 
     def render(self):
         color = pygame.Color(80, 80, 80)
-        for y in range(self.height):
-            for x in range(self.width):
+        for y in range(self.width):
+            for x in range(self.height):
                 pygame.draw.rect(self.screen, color, (
                     self.left + y * self.cell_size, self.top + x * self.cell_size, self.cell_size,
                     self.cell_size), 1)
@@ -41,7 +42,7 @@ class Board:  # отрисовка поля
     def get_cell(self, mouse_position):
         self.cell_x = (mouse_position[0] - self.left) // self.cell_size
         self.cell_y = (mouse_position[1] - self.top) // self.cell_size
-        if (0 <= self.cell_x < self.width) and (0 <= self.cell_y < self.height):
+        if (0 <= self.cell_x < self.height) and (0 <= self.cell_y < self.width):
             print(self.cell_x, self.cell_y)
             return self.cell_x, self.cell_y
 
@@ -50,56 +51,75 @@ class Board:  # отрисовка поля
             self.board[self.cell_y][self.cell_x] = (self.board[self.cell_y][self.cell_x] + 1)
 
 
-class ResidentialBuildings():
-    def __init__(self, *args):
-        pass
-
-
-class IndustrialBuildings():
-    def __init__(self, *args):
-        pass
-        #  сельхоз, заводы
-
-
-class CultureBuildings():
-    def __init__(self, *args):
-        pass
-    #  кружки, театр, парки
-
-
-class EntertainmentBuildings():
-    def __init__(self, *args):
-        pass
-    #  площадки, кино
-
-
-class Road():
-    def __init__(self, *args):
-        pass
-    #  дороги
+# class ResidentialBuildings():
+#     def __init__(self, *args):
+#         pass
+#
+#
+# class IndustrialBuildings():
+#     def __init__(self, *args):
+#         pass
+#         #  сельхоз, заводы
+#
+#
+# class CultureBuildings():
+#     def __init__(self, *args):
+#         pass
+#     #  кружки, театр, парки
+#
+#
+# class EntertainmentBuildings():
+#     def __init__(self, *args):
+#         pass
+#     #  площадки, кино
+#
+#
+# class Road():
+#     def __init__(self, *args):
+#         pass
+#     #  дороги
 
 
 if __name__ == '__main__':
     pygame.init()
-    WIDTH_SIZE, HEIGHT_SIZE = 800, 800
+    WIDTH_SIZE, HEIGHT_SIZE = 1420, 920
     FPS = 60
+    color = 'black'
 
     screen = pygame.display.set_mode((WIDTH_SIZE, HEIGHT_SIZE))
     pygame.display.set_caption('Панельки')
 
-    fon_image = load_image('map.png')
-    fonWidth, fonHeight = fon_image.get_rect().size
-
-    main = Board(screen)
+    board = Board(screen)
     clock = pygame.time.Clock()
 
     running = True
     main_build = True
 
+    manager = pygame_gui.UIManager(
+        (WIDTH_SIZE, HEIGHT_SIZE))  # Обрабатывает функции обновления ПИ, которые мы создаём и передаем ему
+
+    switch = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((10, 10), (100, 40)),  # размеры и положение
+        text='Тест',  # текст
+        manager=manager  # указываем на наш менеджер
+    )
+
     while running:
+        time_delta = clock.tick(FPS) / 1000.0  # некоторые события ПИ используют таймеры
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.USEREVENT:  # указываем на пользоваетльское событие
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:  # указываем на событие нажатие кнопки
+                    if event.ui_element == switch:
+                        if color == 'black':
+                            color = 'gray'
+                        else:
+                            color = 'black'
+                        screen.fill(pygame.Color(color))
+
+            manager.process_events(event)
+        manager.update(time_delta)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:  # Выход из игры
@@ -110,9 +130,12 @@ if __name__ == '__main__':
             if main_build:
                 pass
 
-        screen.blit(fon_image, (0, 0))
-        main.render()
+        screen.fill(pygame.Color(color))
+        manager.draw_ui(screen)
         all_sprites.draw(screen)
+
+        board.render()
+
         pygame.display.flip()
 
     pygame.quit()
